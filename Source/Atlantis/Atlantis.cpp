d197c64c489a3735727ae376f69b20d7e1f33daa
mpstone1231 <michael.stone1231@gmail.com>
2024-10-04 09:07:53 -0400

    Initial Commit

---
+++ Source/Atlantis/Atlantis.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Atlantis.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Atlantis, "Atlantis" );
+
+DEFINE_LOG_CATEGORY(LogAtlantis)
+ 
---
+++ Source/Atlantis/Public/Controller/AtlantisPlayerController.h
@@ -0,0 +1,63 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Templates/SubclassOf.h"
+#include "GameFramework/PlayerController.h"
+#include "AtlantisPlayerController.generated.h"
+
+/** Forward declaration to improve compiling times */
+class UNiagaraSystem;
+class UInputMappingContext;
+class UInputAction;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
+
+UCLASS()
+class AAtlantisPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AAtlantisPlayerController();
+
+	/** Time Threshold to know if it was a short press */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	float ShortPressThreshold;
+
+	/** FX Class that we will spawn when clicking */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UNiagaraSystem* FXCursor;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+	
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputAction* SetDestinationClickAction;
+
+protected:
+	/** True if the controlled character should navigate to the mouse cursor. */
+	uint32 bMoveToMouseCursor : 1;
+
+	virtual void SetupInputComponent() override;
+	
+	// To add mapping context
+	virtual void BeginPlay();
+
+	/** Input handlers for SetDestination action. */
+	void OnInputStarted();
+	void OnSetDestinationTriggered();
+	void OnSetDestinationReleased();
+	void OnTouchTriggered();
+	void OnTouchReleased();
+
+private:
+	FVector CachedDestination;
+
+	float FollowTime; // For how long it has been pressed
+};
+
+
